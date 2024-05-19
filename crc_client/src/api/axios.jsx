142297cf265290103b46d7f40178d2e51366263c
pedro-salinas <pedrosalinas0899@gmail.com>
2024-05-19 18:28:29 -0400

    Update axios.jsx

--- crc_client/src/api/axios.jsx
+++ crc_client/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "http://84.46.239.60:3000/api",
     withCredentials: true,
 });
