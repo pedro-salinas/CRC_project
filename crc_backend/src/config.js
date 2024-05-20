985bd91b7cda0b70b0bfbd079813831d5ef977c8
pedro-salinas <pedrosalinas0899@gmail.com>
2024-05-19 23:25:22 -0400

    gitignore arreglo

--- crc_backend/src/config.js
+++
@@ -1,6 +0,0 @@
-export const TOKEN_SECRET = "extremely secret key omg";
-export const MAIL_USER = "contacto@psalinas.cl";
-export const MAIL_PASSWORD = "Pedro90@salinas";
-export const HOST = "smtp.hostinger.com";
-export const PORT = 465;
-export const URL_FRONTEND = "http://localhost:5173";
