dd1df77e21ba65662ca0700059b94156dd22be73
pedro-salinas <pedrosalinas0899@gmail.com>
2024-05-19 22:11:30 -0400

    Configuraciones

--- crc_backend/src/config.js
+++ crc_backend/src/config.js
@@ -3,4 +3,8 @@ export const MAIL_USER = "contacto@psalinas.cl";
 export const MAIL_PASSWORD = "Pedro90@salinas";
 export const HOST = "smtp.hostinger.com";
 export const PORT = 465;
+<<<<<<< Updated upstream
 export const URL_FRONTEND = "http://localhost:5173";
+=======
+export const URL_FRONTEND = "http://localhost";
+>>>>>>> Stashed changes
