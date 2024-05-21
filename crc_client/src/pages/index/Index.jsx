ccd21dd70133897d8f9fa319bc9e31529e166999
pedro-salinas <pedrosalinas0899@gmail.com>
2024-05-21 19:11:27 -0400

    Update Index.jsx

--- crc_client/src/pages/index/Index.jsx
+++ crc_client/src/pages/index/Index.jsx
@@ -17,17 +17,17 @@ export function Index() {
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '1333188473917527');
-      fbq('track', 'PageView');
-      `,
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '818883853621692');
+          fbq('track', 'PageView');
+        `,
                     }}
                 />
                 {/* End Meta Pixel Code */}
@@ -36,7 +36,7 @@ export function Index() {
                         height="1"
                         width="1"
                         style={{ display: "none" }}
-                        src="https://www.facebook.com/tr?id=1333188473917527&ev=PageView&noscript=1"
+                        src="https://www.facebook.com/tr?id=818883853621692&ev=PageView&noscript=1"
                     />
                 </noscript>
             </Fragment>
