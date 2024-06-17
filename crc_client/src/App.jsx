1909065c388a955fef1c3c7bff7cbf2067384032
pedro-salinas <pedrosalinas0899@gmail.com>
2024-06-17 00:12:19 -0400

    Update App.jsx

--- crc_client/src/App.jsx
+++ crc_client/src/App.jsx
@@ -37,11 +37,15 @@ function App() {
     // Redireccionar de www a non-www
     const [redirected, setRedirected] = useState(false);
 
+    console.log(VITE_URL);
+    console.log(VITE_PROTOCOL);
+
     useEffect(() => {
         if (window.location.hostname === "www" + VITE_URL) {
             setRedirected(true);
             window.location.href =
                 VITE_PROTOCOL + VITE_URL + window.location.pathname;
+            console.log(VITE_PROTOCOL + VITE_URL + window.location.pathname);
         }
     }, []);
 
